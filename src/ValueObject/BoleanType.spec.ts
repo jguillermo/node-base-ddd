09ec65b5dbde15b4c96182a8ff34197d1a4ea514
Jose Guillermo <jguillermo@outlook.com>
2021-06-10 00:53:19 -0500

    feat(vo): vo boolean contructor has filter value

--- src/ValueObject/BoleanType.spec.ts
+++ src/ValueObject/BoleanType.spec.ts
@@ -1,8 +1,8 @@
 import { BooleanTypeImp } from './Implement/BooleanTypeImp';
+import { ValueGenerator } from './base.spec';
 
 describe('String Type', () => {
   let type: BooleanTypeImp;
-
   describe('constructor set values', () => {
     describe('valid boolean', () => {
       it('true', () => {
@@ -24,6 +24,50 @@ describe('String Type', () => {
       it('undefined equals null', () => {
         type = new BooleanTypeImp(undefined);
         expect(type.value).toEqual(null);
+      });
+    });
+    describe('set invalid value', () => {
+      describe('string', () => {
+        it('empty', () => {
+          type = new BooleanTypeImp(ValueGenerator.valueString(''));
+          expect(type.value).toEqual(false);
+        });
+        it('true', () => {
+          type = new BooleanTypeImp(ValueGenerator.valueString('true'));
+          expect(type.value).toEqual(true);
+        });
+        it('false', () => {
+          type = new BooleanTypeImp(ValueGenerator.valueString('false'));
+          expect(type.value).toEqual(false);
+        });
+        it('random', () => {
+          type = new BooleanTypeImp(ValueGenerator.valueString('random'));
+          expect(type.value).toEqual(true);
+        });
+      });
+      describe('number', () => {
+        it('positive', () => {
+          type = new BooleanTypeImp(ValueGenerator.valueBooleanNumber(1));
+          expect(type.value).toEqual(true);
+          type = new BooleanTypeImp(ValueGenerator.valueBooleanNumber(1.1));
+          expect(type.value).toEqual(true);
+          type = new BooleanTypeImp(ValueGenerator.valueBooleanNumber(0.1));
+          expect(type.value).toEqual(true);
+        });
+
+        it('negative', () => {
+          type = new BooleanTypeImp(ValueGenerator.valueBooleanNumber(-1));
+          expect(type.value).toEqual(true);
+          type = new BooleanTypeImp(ValueGenerator.valueBooleanNumber(-1.1));
+          expect(type.value).toEqual(true);
+          type = new BooleanTypeImp(ValueGenerator.valueBooleanNumber(-0.1));
+          expect(type.value).toEqual(true);
+        });
+
+        it('zero', () => {
+          type = new BooleanTypeImp(ValueGenerator.valueBooleanNumber(0));
+          expect(type.value).toEqual(false);
+        });
       });
     });
   });
