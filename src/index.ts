6997ed4b3a819d07b6337bf9c43e078c9e3a7528
Jose Guillermo <jguillermo@outlook.com>
2021-06-13 06:05:12 -0500

    feat(vo): implement uuid type

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,7 @@ export * from './ValueObject/DateType';
 export * from './ValueObject/EnumType';
 export * from './ValueObject/NumberType';
 export * from './ValueObject/StringType';
+export * from './ValueObject/UUIDType';
+
+//value object implement
+export * from './ValueObject/Implement/UUIDTypeImp';
