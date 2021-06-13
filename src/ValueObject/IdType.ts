704f111b52694d6c5dc0eb547e7692d8e4c91ae8
Jose Guillermo <jguillermo@outlook.com>
2021-06-13 06:19:17 -0500

    feat(vo): add Id Type

---
+++ src/ValueObject/IdType.ts
@@ -0,0 +1,10 @@
+import { UUIDType } from './UUIDType';
+
+export abstract class IdType extends UUIDType {
+  protected filter(value: any): string | null {
+    if (value === null) {
+      throw new Error(`Id is required`);
+    }
+    return super.filter(value);
+  }
+}
