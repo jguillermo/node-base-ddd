2f6d5e12b8ee065b4290c2f0b4f76dbe8ae50ec4
Jose Guillermo <jguillermo@outlook.com>
2021-06-13 06:20:55 -0500

    fix(vo): add import IdType

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 export * from './ValueObject/BooleanType';
 export * from './ValueObject/DateType';
 export * from './ValueObject/EnumType';
+export * from './ValueObject/IdType';
 export * from './ValueObject/NumberType';
 export * from './ValueObject/StringType';
 export * from './ValueObject/UUIDType';
