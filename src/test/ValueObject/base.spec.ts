652bc737837d8ef47aba65fb309af5302795b738
Jose Guillermo <jguillermo@outlook.com>
2021-07-28 22:46:44 -0500

    feat(type): add is not value base class

--- src/test/ValueObject/base.spec.ts
+++ src/test/ValueObject/base.spec.ts
@@ -1,3 +1,5 @@
+import { BaseType } from '../../ValueObject/BaseType';
+
 export class ValueGenerator {
   static valueString(value: string): any {
     return value;
@@ -12,6 +14,22 @@ export class ValueGenerator {
   }
 }
 
+class TestBaseType extends BaseType<string> {
+  get toString(): string {
+    if (this.isNull) {
+      return '';
+    }
+    return <string>this.value;
+  }
+
+  protected filter(value: any): string | null {
+    if (value === null) {
+      return null;
+    }
+    return `${value}`;
+  }
+}
+
 describe('String Type', () => {
   it('should be', () => {
     expect(ValueGenerator.valueString('string')).toEqual('string');
@@ -19,5 +37,16 @@ describe('String Type', () => {
     expect(ValueGenerator.valueBoolean(false)).toEqual(false);
     expect(ValueGenerator.valueNumber(0)).toEqual(0);
     expect(ValueGenerator.valueNumber(1)).toEqual(1);
+  });
+});
+
+describe('TestBaseType', () => {
+  it('is null', () => {
+    const object = new TestBaseType();
+    expect(object.isNull).toEqual(true);
+  });
+  it('is not null', () => {
+    const object = new TestBaseType();
+    expect(object.isNotNull).toEqual(false);
   });
 });
