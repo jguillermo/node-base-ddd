45ed3822c432e586aeaa1db44b75e50224febe7d
Jose Guillermo <jguillermo@outlook.com>
2021-07-28 22:50:28 -0500

    feat(export): add baseType and typeImp in export

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,13 @@ export * from './ValueObject/IdType';
 export * from './ValueObject/NumberType';
 export * from './ValueObject/StringType';
 export * from './ValueObject/UUIDType';
+export * from './ValueObject/BaseType';
 
 //value object implement
+export * from './ValueObject/Implement/BooleanTypeImp';
+export * from './ValueObject/Implement/DateTypeImp';
+export * from './ValueObject/Implement/EnumTypeImp';
+export * from './ValueObject/Implement/IdTypeImp';
+export * from './ValueObject/Implement/NumberTypeImp';
+export * from './ValueObject/Implement/StringTypeImp';
 export * from './ValueObject/Implement/UUIDTypeImp';
