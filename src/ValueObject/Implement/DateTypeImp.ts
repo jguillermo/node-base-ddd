d3625a2cc7c031e6915ffa53906478cb5f900b95
Jose Guillermo <jguillermo@outlook.com>
2021-09-06 17:05:23 -0500

    fix(vo): add statuc method in vo date

--- src/ValueObject/Implement/DateTypeImp.ts
+++ src/ValueObject/Implement/DateTypeImp.ts
@@ -1,3 +1,7 @@
 import { DateType } from '../DateType';
 
-export class DateTypeImp extends DateType {}
+export class DateTypeImp extends DateType {
+  static create(value: any = null) {
+    return new DateTypeImp(value);
+  }
+}
